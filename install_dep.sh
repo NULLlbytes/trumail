7badb4bc20e5b0c9219d0cbd95a14fbb3f48f76d
Ashwani Agarwal <ashwani.a@outlook.com>
2019-03-12 17:03:10 +0530

    updated install_dep

--- install_dep.sh
+++ install_dep.sh
@@ -17,10 +17,14 @@ fi
 
 go get github.com/gorilla/handlers
 go get github.com/gorilla/mux
-github.com/sirupsen/logrus
 go get github.com/sirupsen/logrus
-golang.org/x/net
-go get golang.org/x/net
+go get golang.org/x/net >> /dev/null 2&> /dev/null
 git clone $GOPATH/src/golang.org/x/net
-go get golang.org/x/sync
+go get golang.org/x/sync >> /dev/null 2&> /dev/null
 git clone $GOPATH/src/golang.org/x/sync
+
+go get -u golang.org/x/text/secure/bidirule
+go get -u golang.org/x/text/unicode/...
+
+echo "Installed all Deps, run with "
+echo "  go run main.go"
